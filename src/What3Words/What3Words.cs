c0e90b2d280579a43a7b49c49c458626218ebd72
Nick Kirby <nick@remote.online>
2019-08-20 12:31:06 +0100

    Pass api key in request headers

--- src/What3Words/What3Words.cs
+++ src/What3Words/What3Words.cs
@@ -9,21 +9,25 @@ namespace What3Words
     public class What3Words : IWhat3Words
     {
         private const string ApiBaseUrl = "https://api.what3words.com/v3/";
-        private static string _apiKey;
         private static HttpClient _httpClient;
 
         public What3Words(string apiKey)
         {
-            _apiKey = apiKey ?? throw new NullReferenceException("API key must not be null");
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new NullReferenceException("API key must not be null");
+            }
+
             _httpClient = new HttpClient
             {
                 BaseAddress = new Uri(ApiBaseUrl)
             };
+            _httpClient.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
         }
 
         public async Task<What3WordsResponse> ConvertToCoordinates(string firstWord, string secondWord, string thirdWord)
         {
-            var response = await _httpClient.GetAsync($"convert-to-coordinates?words={firstWord}.{secondWord}.{thirdWord}&key={_apiKey}");
+            var response = await _httpClient.GetAsync($"convert-to-coordinates?words={firstWord}.{secondWord}.{thirdWord}");
             if (!response.IsSuccessStatusCode)
                 HandleUnsuccessfulResponse(response);
 
@@ -33,7 +37,7 @@ namespace What3Words
 
         public async Task<What3WordsResponse> ConvertTo3wa(double lat, double lng)
         {
-            var response = await _httpClient.GetAsync($"convert-to-3wa?coordinates={lat},{lng}&key={_apiKey}");
+            var response = await _httpClient.GetAsync($"convert-to-3wa?coordinates={lat},{lng}");
             if (!response.IsSuccessStatusCode)
                 HandleUnsuccessfulResponse(response);
 
